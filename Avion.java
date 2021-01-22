c117a535e0f1f8a7c7502b84145c15f2c2c42109
adolfopalma <adolfo.palmagr@gmail.com>
2021-01-22 18:01:13 +0100

    Fix clase avion

--- Avion.java
+++ Avion.java
@@ -1,8 +1,7 @@
-public class Avion extends Vehiculo implements Sumergible {
+public class Avion extends Vehiculo {
     int z;
     boolean despegado;
     String medio = "Tierra";
-    String est = "Sin inmersion";
 
     public Avion() {
         z = 0;
@@ -42,41 +41,6 @@ public class Avion extends Vehiculo implements Sumergible {
         }
     }
 
-    public void cambioDeMedio() {
-        if (this.getZ() == 0) {
-            if (this.getMedio() == "Tierra") {
-                this.setMedio("Agua");
-            }
-            if (this.getMedio() == "Agua") {
-                this.setMedio("Tierra");
-            }
-        }
-    }
-
-    public void inmersion() {
-        if (this.estado && this.medio == "Agua") {
-            this.setEst("Inmersion");
-            this.setZ(-1);
-        }
-
-    }
-
-    public void ascender(int n) {
-        if (this.estado && this.medio == "Agua" && this.est == "Inmersion") {
-            z = z + n;
-            if (z >= 0) {
-                this.setEst("Sin inmesion");
-                this.setZ(0);
-            }
-        }
-    }
-
-    public void descender(int n) {
-        if (this.estado && this.medio == "Agua" && this.est == "Inmersion") {
-            z = z - n;
-        }
-    }
-
     public int getZ() {
         return this.z;
     }
@@ -103,13 +67,5 @@ public class Avion extends Vehiculo implements Sumergible {
 
     public void setMedio(String medio) {
         this.medio = medio;
-    }
-
-    public String getEst() {
-        return this.est;
-    }
-
-    public void setEst(String est) {
-        this.est = est;
     }
 }
